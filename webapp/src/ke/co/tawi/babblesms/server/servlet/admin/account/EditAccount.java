a9cfcab956baef0dff16262c3d6f79a334154c15
peter <mwendapeter72@gmail.com>
2015-06-04 12:33:27 +0300

    style.css and EditAccount.java

---
+++ webapp/src/ke/co/tawi/babblesms/server/servlet/admin/account/EditAccount.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright 2015 Tawi Commercial Services Ltd
+ * 
+ * Licensed under the Open Software License, Version 3.0 (the “License”); you may
+ * not use this file except in compliance with the License. You may obtain a copy
+ * of the License at:
+ * http://opensource.org/licenses/OSL-3.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package ke.co.tawi.babblesms.server.servlet.admin.account;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import ke.co.tawi.babblesms.server.accountmgmt.admin.SessionConstants;
+import ke.co.tawi.babblesms.server.beans.account.Account;
+import ke.co.tawi.babblesms.server.cache.CacheVariables;
+import ke.co.tawi.babblesms.server.persistence.accounts.AccountDAO;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+/**
+ * 
+ *Recieves data from editaccount.jsp
+ *  {@link EditAccount} and save changes to the database.
+ * @author <a href="mailto:michael@tawi.mobi">Michael Wakahe</a>
+ */
+public class EditAccount extends HttpServlet {
+
+	
+	
+	
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private AccountDAO accountDAO;
+    private CacheManager cacheManager;
+    
+    
+    
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+       
+        accountDAO = AccountDAO.getInstance();
+
+        cacheManager = CacheManager.getInstance();
+    }
+    
+    /**
+	 *
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+    /**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	   HttpSession session = request.getSession(false);
+    	
+    	String accountuuid = request.getParameter("accountuuid");
+        String username = request.getParameter("username");
+        String password = request.getParameter("loginPasswd");
+        String name = request.getParameter("name");
+        String mobile = request.getParameter("mobile");
+        String email = request.getParameter("email");
+      
+
+        Account account = new Account();
+        account.setUuid(accountuuid);
+        account.setUsername(username);
+        account.setLogpassword(password);
+        account.setName(name);
+        account.setMobile(mobile);
+        account.setEmail(email);
+        
+        updateAccountCache(account);
+      
+        if(accountDAO.updateAccount(accountuuid, account)){
+            session.setAttribute(SessionConstants.ADMIN_UPDATE_SUCCESS, "Account updated successfully.");
+        } else {
+            session.setAttribute(SessionConstants.ADMIN_UPDATE_ERROR, "Account update failed."); 
+          
+            
+  }
+        
+
+        response.sendRedirect("admin/accounts.jsp");
+
+ 
+    	
+    }
+    
+    
+    private void updateAccountCache(Account acc) {
+        cacheManager.getCache(CacheVariables.CACHE_ACCOUNTS_BY_USERNAME).put(new Element(acc.getUsername(), acc));
+        cacheManager.getCache(CacheVariables.CACHE_ACCOUNTS_BY_UUID).put(new Element(acc.getUuid(), acc));
+    }
+    
+   
+    /**
+	 *
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+    
+    
+
+
+}
