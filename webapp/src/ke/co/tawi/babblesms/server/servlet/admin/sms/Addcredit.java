79c638654eeb4df8f7e6e94193c28e2ddd9a8b9c
Peter Mwenda <mwendapeter72@gmail.com>
2015-06-30 08:36:05 +0300

    changes to addcredit

--- webapp/src/ke/co/tawi/babblesms/server/servlet/admin/sms/Addcredit.java
+++ webapp/src/ke/co/tawi/babblesms/server/servlet/admin/sms/Addcredit.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2015 Tawi Commercial Services Ltd
+ * 
+ * Licensed under the Open Software License, Version 3.0 (the “License”); you may
+ * not use this file except in compliance with the License. You may obtain a copy
+ * of the License at:
+ * http://opensource.org/licenses/OSL-3.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package ke.co.tawi.babblesms.server.servlet.admin.sms;
 
 import ke.co.tawi.babblesms.server.accountmgmt.admin.SessionConstants;
@@ -26,7 +41,7 @@ import org.apache.commons.lang3.StringUtils;
  * Servlet used to add account credit.
  * <p>
  * Copyright (c) Tawi Ltd., July 7,2014
- *@author <a href="mailto:michael@tawi.mobi">peter</a>
+ *@author <a href="mailto:michael@tawi.mobi">Michael Wakahe</a>
  * @author <a href="mailto:mwenda@tawi.mobi">peter</a>
  */
 public class Addcredit extends HttpServlet {
@@ -39,6 +54,8 @@ public class Addcredit extends HttpServlet {
     final String ERROR_NO_AMOUNT = "Please provide a value of client credits that are greater than zero.";
     final String ERROR_INVALID_AMOUNT = "Please provide a numeric value of client credits that are greater than zero.";
     final String ERROR_INVALID_SOURCE = "Please provide a source that belongs to the account.";
+    
+    private String amount,sourceuuid,accountuuid;
     private final MaskDAO maskDAO = MaskDAO.getInstance();
     private final ShortcodeDAO scodeDAO = ShortcodeDAO.getInstance();
     private SmsPurchaseDAO smspurchaseDAO;
@@ -73,16 +90,15 @@ public class Addcredit extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         session = request.getSession(true);
-        long Dates = System.currentTimeMillis();
-       
-       String amount = StringUtils.trimToEmpty(request.getParameter("amount"));
+        setClassParameters(request);
+        
+       /*String amount = StringUtils.trimToEmpty(request.getParameter("amount"));
        String sourceuuid = StringUtils.trimToEmpty(request.getParameter("source")); 
-       String  accountuuid = StringUtils.trimToEmpty(request.getParameter("user"));
+       String  accountuuid = StringUtils.trimToEmpty(request.getParameter("user"));*/
       
       // System.out.println(sourceuuid);
       // System.out.println(accountuuid);
-       
-       int count = Integer.parseInt(amount);
+        
       // System.out.println(count);
         // No source provided
         if (StringUtils.isBlank(sourceuuid)) {
@@ -101,51 +117,74 @@ public class Addcredit extends HttpServlet {
             session.setAttribute(SessionConstants.ADMIN_ADD_CREDIT_ERROR_KEY, ERROR_NO_ACCOUNTNAME);
 
         } else {
-        	//this lists holds mask details
-        	List<Mask> maskList = maskDAO.getmaskbyaccount(accountuuid);
-        	//this lists holds shortcode details
-        	List<Shortcode> shortcodeList = scodeDAO.getShortcodebyaccountuuid(accountuuid);
-	       
-        	    
-                  //loop via masks and check if source is a mask
-                 for(Mask ma : maskList){
-                	String maskuuid = ma.getUuid();
-                	if(sourceuuid.equals(maskuuid)){
-                	  maskp.setAccountUuid(accountuuid);
-   	                  maskp.setCount(count); 
-   	                  maskp.setPurchaseDate(new Date(new Long(Dates)));
-   	                  maskp.setSourceUuid(maskuuid); 
-   	                 smspurchaseDAO.put(maskp);
-   	              //success message here
-                    session.setAttribute(SessionConstants.ADMIN_UPDATE_SUCCESS, "Credit updated successfully.");
-   	             // System.out.println(maskuuid);
-                	}
-                	 
-                 }
-                 //loop via shortcodes and check if the source is a shortcode
-                for(Shortcode code : shortcodeList)  {
-                	String codeuuid = code.getUuid();
-                	if(sourceuuid.equals(codeuuid)){
-                	  shortcodep.setAccountUuid(accountuuid);
-  		              shortcodep.setPurchaseDate(new Date(new Long(Dates)));
-  		              shortcodep.setCount(count);
-  		              shortcodep.setSourceUuid(codeuuid); 
-  		                smspurchaseDAO.put(shortcodep);
-  		              //System.out.println(sourceuuid);
-  	   	             // System.out.println(codeuuid);
-  		              //success message here
-  	                 session.setAttribute(SessionConstants.ADMIN_UPDATE_SUCCESS, "Credit updated successfully.");
-                	}
-                	 
-                }
+        	 // If we get this far then all parameter checks are ok.		
+            session.setAttribute(SessionConstants.ADMIN_ADD_CREDIT_SUCCESS_KEY, "s");
+
+            // Reduce our session data
+            session.setAttribute(SessionConstants.ADMIN_ADD_CREDIT_PARAMETERS, null);
+            session.setAttribute(SessionConstants.ADMIN_ADD_CREDIT_ERROR_KEY, null);
+
+               getSource();
+            
+              //success message here
+                session.setAttribute(SessionConstants.ADMIN_UPDATE_SUCCESS, "Credit updated successfully."); 
            
         	}
         	 //err message and redirect url
         		response.sendRedirect("admin/credit.jsp");
-                session.setAttribute(SessionConstants.ADMIN_UPDATE_ERROR, "Credit update failed.");	
+               // session.setAttribute(SessionConstants.ADMIN_UPDATE_ERROR, "Credit update failed.");	
          
          
         }
+
+	private void getSource() {
+		   long Dates = System.currentTimeMillis();
+	       int count = Integer.parseInt(amount);
+		//this lists holds mask details
+    	List<Mask> maskList = maskDAO.getmaskbyaccount(accountuuid);
+    	//this lists holds shortcode details
+    	List<Shortcode> shortcodeList = scodeDAO.getShortcodebyaccountuuid(accountuuid);
+       
+    	    
+              //loop via masks and check if source is a mask
+             for(Mask ma : maskList){
+            	String maskuuid = ma.getUuid();
+            	if(sourceuuid.equals(maskuuid)){
+            	      maskp.setAccountUuid(accountuuid);
+	                  maskp.setCount(count); 
+	                  maskp.setPurchaseDate(new Date(new Long(Dates)));
+	                  maskp.setSourceUuid(maskuuid); 
+	                if( smspurchaseDAO.put(maskp)){
+	                	//session.setAttribute(SessionConstants.ADMIN_UPDATE_SUCCESS, "Credit updated successfully.");
+	                }
+	              
+	             // System.out.println(maskuuid);
+            	}
+            	 
+             }
+             //loop via shortcodes and check if the source is a shortcode
+            for(Shortcode code : shortcodeList)  {
+            	String codeuuid = code.getUuid();
+            	if(sourceuuid.equals(codeuuid)){
+            	  shortcodep.setAccountUuid(accountuuid);
+		              shortcodep.setPurchaseDate(new Date(new Long(Dates)));
+		              shortcodep.setCount(count);
+		              shortcodep.setSourceUuid(codeuuid); 
+		               if(smspurchaseDAO.put(shortcodep)){	            
+	                 session.setAttribute(SessionConstants.ADMIN_UPDATE_SUCCESS, "Credit updated successfully.");
+            	}
+		               }
+            }
+	}
+
+
+	private void setClassParameters(HttpServletRequest request) {
+		   amount = StringUtils.trimToEmpty(request.getParameter("amount"));
+	       sourceuuid = StringUtils.trimToEmpty(request.getParameter("source")); 
+	       accountuuid = StringUtils.trimToEmpty(request.getParameter("user"));
+		
+	}
+
 
 	/**
      *
